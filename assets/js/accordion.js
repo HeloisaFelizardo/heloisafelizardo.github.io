54d486bb8fa04b701cee547efd6f55882901a97f
Heloisa Felizardo <helocmp@yahoo.com.br>
2023-07-15 23:15:18 -0300

    feat: finalização das seções

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -3,12 +3,19 @@ const accordionTrigger = document.querySelectorAll('.accordion .trigger');
 accordionTrigger.forEach((trigger) => {
 	trigger.addEventListener('click', (e) => {
 		const accordion = trigger.parentElement;
+		const content = accordion.querySelector('.content');
 		const isOpen = accordion.classList.contains('open');
-		if (isOpen) {
-			accordion.classList.remove('open');
-		} else {
+
+		// Remove a classe 'open' de todos os acordeões abertos
+		document.querySelectorAll('.accordion.open').forEach((openAccordion) => {
+			openAccordion.classList.remove('open');
+			openAccordion.querySelector('.content').style.maxHeight = null;
+		});
+
+		if (!isOpen) {
 			accordion.classList.add('open');
-			accordion.classList.add('transition'); // Adiciona a classe 'transition' ao abrir o accordion
+			accordion.classList.add('transition');
+			content.style.maxHeight = content.scrollHeight + 'px';
 		}
 	});
 });
