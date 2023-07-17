fec9bb1b0e96b7379025fd919fcea973993f5f15
Heloisa Felizardo <helocmp@yahoo.com.br>
2023-07-16 23:20:52 -0300

    feat: adicionando text effect load no titulo

--- assets/js/textLoad.js
+++ assets/js/textLoad.js
@@ -1,16 +1,49 @@
-const text = document.querySelector('.header .title');
+const text = document.querySelector('.header .title span');
+const words = ['Heloisa Felizardo!', 'Web Developer.', 'Freelancer.', 'Designer.'];
+let index = 0;
 
+// Função principal para carregar e apagar o texto
 const textLoad = () => {
-	setTimeout(() => {
-		text.textContent = 'Developer';
-	}, 0);
-	setTimeout(() => {
-		text.textContent = 'Freelancer';
-	}, 4000);
-	setTimeout(() => {
-		text.textContent = 'Designer';
-	}, 8000);
+	const word = words[index]; // Obtém a palavra atual do array 'words'
+	let currentText = ''; // Texto atual exibido no título
+	let i = 0; // Índice para percorrer os caracteres da palavra
+
+	// Timer para adicionar um caractere por vez
+	const timer = setInterval(() => {
+		currentText += word[i]; // Adiciona o próximo caractere à palavra atual
+		text.textContent = currentText; // Atualiza o texto exibido no título
+		i++; // Incrementa o índice para o próximo caractere
+
+		// Verifica se todos os caracteres foram exibidos
+		if (i === word.length) {
+			clearInterval(timer); // Limpa o timer
+
+			// Inicia o processo de apagar o texto após um pequeno intervalo
+			setTimeout(() => {
+				eraseText();
+			}, 1000);
+		}
+	}, 100);
+
+	// Função para apagar o texto
+	const eraseText = () => {
+		// Timer para remover um caractere por vez
+		const eraseTimer = setInterval(() => {
+			currentText = currentText.slice(0, -1); // Remove o último caractere do texto atual
+			text.textContent = currentText; // Atualiza o texto exibido no título
+
+			// Verifica se todo o texto foi removido
+			if (currentText === '') {
+				clearInterval(eraseTimer); // Limpa o timer
+
+				index = (index + 1) % words.length; // Atualiza o índice para a próxima palavra
+				// Chama a função textLoad novamente após um pequeno intervalo
+				setTimeout(() => {
+					textLoad();
+				}, 1000);
+			}
+		}, 100);
+	};
 };
 
-/* textLoad();
-setInterval(textLoad, 1200); */
+textLoad(); // Inicia o carregamento do texto no título
