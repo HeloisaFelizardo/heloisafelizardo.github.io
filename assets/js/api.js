5293ad637f4f15b26cef62df3990842d9ee3300f
Heloisa Felizardo <helocmp@yahoo.com.br>
2023-07-21 13:06:14 -0300

    feat: created fetch method

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,3 +1,5 @@
-function fetchProfileData() {
-	const url = 'https://raw.githubusercontent.com/HeloisaFelizardo/heloisafelizardo.github.io/main/data/profile.json';
+async function fetchProfileData() {
+	await fetch('https://raw.githubusercontent.com/HeloisaFelizardo/heloisafelizardo.github.io/main/data/profile.json')
+		.then((response) => response.json())
+		.then((data) => console.log(data));
 }
